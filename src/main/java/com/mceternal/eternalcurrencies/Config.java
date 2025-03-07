fa56924fe75d57edfd441712cb38c71909d17ce8
anabsolutesloth <63694982+anabsolutesloth@users.noreply.github.com>
2025-03-06 17:26:45 -0800

    Initial Commit.

---
+++ src/main/java/com/mceternal/eternalcurrencies/Config.java
@@ -0,0 +1,14 @@
+package com.mceternal.eternalcurrencies;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.common.Mod;
+
+// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
+// Demonstrates how to use Forge's config APIs
+@Mod.EventBusSubscriber(modid = EternalCurrencies.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config {
+
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+    static final ForgeConfigSpec SPEC = BUILDER.build();
+}
